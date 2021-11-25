4755fbc0a42a5a9d2e46abdb790691cc1614b77c
Desiree Mendes <desireefrost@live.ca>
2021-11-25 15:07:09 -0500

    getAllReservations update to use DB with SQL

--- BnB_WebApp/server/database.js
+++ BnB_WebApp/server/database.js
@@ -63,7 +63,21 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+    SELECT reservations.*, properties.*, avg(rating) as average_rating
+      FROM reservations
+      JOIN properties ON reservations.property_id = properties.id
+      JOIN property_reviews ON properties.id = property_reviews.property_id
+      WHERE reservations.guest_id = $1
+        AND reservations.end_date < now()::date
+      GROUP BY properties.id, reservations.id
+      ORDER BY reservations.start_date
+      LIMIT $2;
+    `,
+    [guestId, limit])
+    .then(res => res.rows)
+    .catch(null);
 }
 exports.getAllReservations = getAllReservations;
 
