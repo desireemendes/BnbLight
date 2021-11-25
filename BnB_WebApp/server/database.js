022ad72061fe4e4c708c4b0d70fd8928cd8685ed
Desiree Mendes <desireefrost@live.ca>
2021-11-25 16:44:28 -0500

    update functions to use SQL

--- BnB_WebApp/server/database.js
+++ BnB_WebApp/server/database.js
@@ -89,13 +89,29 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
- const getAllProperties = (options, limit = 10) => {
+ const getAllProperties = function(options, limit = 10) {
+  const insertParam = function(param, refArr, strBef, strAft) {
+    if (!param) return "";
+    if (strBef.slice(strBef.length - 4) === 'LIKE') param = `%${param}%`;
+    refArr.push(param);
+    return `${strBef} $${refArr.length} ${strAft}`;
+  };
+  const params = [];
+  let queryString = `SELECT properties.*, avg(rating) AS average_rating
+      FROM properties
+      JOIN property_reviews ON properties.id = property_id
+      ${insertParam(options.city, params, 'WHERE city LIKE','')}
+      ${insertParam(options.owner_id, params, params.length ? 'AND owner_id =' : 'WHERE owner_id =','')}
+      ${insertParam(options.minimum_price_per_night * 100, params, params.length ? 'AND cost_per_night >=' : 'WHERE cost_per_night >=','')}
+      ${insertParam(options.maximum_price_per_night * 100, params, params.length ? 'AND cost_per_night <=' : 'WHERE cost_per_night <=','')}
+      GROUP BY properties.id
+      ${insertParam(options.minimum_rating, params, 'HAVING avg(rating) >=','')}
+      ${insertParam(limit, params, 'LIMIT','')};`;
+      
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => result.rows)
-    .catch((err) => {
-      console.log(err.message);
-    });
+    .query(queryString, params)
+    .then(res => res.rows)
+    .catch((err) => err.message);
 };
 exports.getAllProperties = getAllProperties;
 
@@ -105,10 +121,22 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+ const addProperty = function(property) {
+  const insertValue = function(value, refArr) {
+    refArr.push(value);
+    return `$${refArr.length}`;
+  };
+  const params = [];
+  const queryString = `
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
+  VALUES
+  (${insertValue(property.owner_id, params)},${insertValue(property.title, params)},${insertValue(property.description, params)},
+  ${insertValue(property.thumbnail_photo_url, params)}, ${insertValue(property.cover_photo_url, params)},
+  ${insertValue(property.cost_per_night, params)}, ${insertValue(property.street, params)}, ${insertValue(property.city, params)},
+  ${insertValue(property.province, params)}, ${insertValue(property.post_code, params)}, ${insertValue(property.country, params)}, 
+  ${insertValue(property.parking_spaces, params)}, ${insertValue(property.number_of_bathrooms, params)}, 
+  ${insertValue(property.number_of_bedrooms, params)})
+  RETURNING *;`;
+  return pool.query(queryString, params); // <<- only the promise
+};
 exports.addProperty = addProperty;
